c699f6fc72345f70a2c8958fa1018af049604d36
118159zjw <zjw118>
2021-04-21 15:33:20 +0800

    微服务1连接Eureka

---
+++ src/main/java/com/cloud/nacos/ComsumerClientApplication.java
@@ -0,0 +1,28 @@
+package com.cloud.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+/**
+ * 启动类
+ *
+ * @author Zhaojingwei
+ * @date 2021/3/18
+ */
+
+@SpringBootApplication
+@ServletComponentScan
+@EnableDiscoveryClient
+@RefreshScope//开启配置更新功能
+public class ComsumerClientApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComsumerClientApplication.class, args);
+
+    }
+
+}
---
+++ src/main/java/com/cloud/nacos/pro/controller/HelloController.java
@@ -0,0 +1,40 @@
+package com.cloud.nacos.pro.controller;
+
+import com.cloud.nacos.common.result.RestfulResult;
+import com.cloud.nacos.common.utils.CommUtils;
+import com.cloud.nacos.pro.entity.ServiceInfo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 微服务控制器
+ *
+ * @author Zhaojingwei
+ * @date 2021/3/18
+ */
+@RestController
+@RequestMapping("service")
+public class HelloController {
+
+    @RequestMapping( "hello")
+    public void login(HttpServletResponse response, HttpServletRequest request, @RequestBody ServiceInfo serviceInfo) {
+
+        RestfulResult restfulResult = new RestfulResult();
+
+        try {
+            restfulResult.setData("Service1:Welcome " + serviceInfo.getName() + "!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        CommUtils.printDataJason(response, restfulResult);
+    }
+
+
+}
